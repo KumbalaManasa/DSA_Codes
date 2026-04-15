9c08c118c4952f3ffab7d7104d0fe19824bcfe61
Kumbala Manasa <kumbalamanasa63@gmail.com>
2026-04-15 16:19:28 +0530

    commit Brute force

---
+++ Remove_outermost_parenthesis_STR.py
@@ -0,0 +1,18 @@
+#brute force
+#We only add parentheses to the result when we are NOT at the outermost level.
+#As long as level > 0, we are insideAny ( or ) here → ✅ include
+def rem_outer_parenthesis(s):
+    result = ""
+    level = 0
+    for chr in s:
+        if chr == "(":
+            if level>0:
+                result+=chr
+            level+=1
+        elif chr == ")":
+            level-=1
+            if level>0:
+                result+=chr
+    return result
+s = "(()())(())"
+print(rem_outer_parenthesis(s))                    
