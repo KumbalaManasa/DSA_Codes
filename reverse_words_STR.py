d75b76783073592d11d29005f2a4042a5f5add39
KumbalaManasa <kumbalamanasa63@gmail.com>
2026-04-15 17:15:06 +0530

    Update reverse_words_STR.py

--- reverse_words_STR.py
+++ reverse_words_STR.py
@@ -1,4 +1,25 @@
-#brute force
+#optimal Approach 
+def reverse_words(s):
+    result = ""
+    i = len(s)-1
+    while i>=0:
+        while i>=0 and s[i]=" ":
+            i-=1
+        if i<0:
+            break
+        end = i
+        while i>=0 qnd s[i]!=" ":
+            i-=1
+        word = s[i+1:end+1]
+        if result!=""
+            result+=" "
+        result+=word
+    return result    
+ s = " amazing coding skills "
+ print(reverse_words(s))
+
+
+'''#brute force
 def rev_words(s):
     word = ""
     words = []
@@ -15,4 +36,4 @@ def rev_words(s):
     words.reverse()
     return " ".join(words)
 s = "hello im kumbala manasa"
-print(rev_words(s))  
+print(rev_words(s))  '''
