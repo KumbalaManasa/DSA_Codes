dea223d888ba1e841a65d9724416feec27c99a73
Kumbala Manasa <kumbalamanasa63@gmail.com>
2026-04-15 17:17:22 +0530

    optimal code reverse words

--- reverse_words_STR.py
+++ reverse_words_STR.py
@@ -3,20 +3,20 @@ def reverse_words(s):
     result = ""
     i = len(s)-1
     while i>=0:
-        while i>=0 and s[i]=" ":
+        while i>=0 and s[i]==" ":
             i-=1
         if i<0:
             break
         end = i
-        while i>=0 qnd s[i]!=" ":
+        while i>=0 and s[i]!=" ":
             i-=1
         word = s[i+1:end+1]
-        if result!=""
+        if result!="":
             result+=" "
         result+=word
     return result    
- s = " amazing coding skills "
- print(reverse_words(s))
+s = " amazing coding skills "
+print(reverse_words(s))
 
 
 '''#brute force
